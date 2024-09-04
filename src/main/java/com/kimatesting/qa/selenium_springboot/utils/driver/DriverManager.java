b6c9746aef4e77bcf83984c96d7df3a29b423c4f
manukippes <emmanuelkippes@gmail.com>
2024-09-04 10:17:00 +0200

    Feat: Add remote execution

--- src/main/java/com/kimatesting/qa/selenium_springboot/utils/driver/DriverManager.java
+++ src/main/java/com/kimatesting/qa/selenium_springboot/utils/driver/DriverManager.java
@@ -5,13 +5,19 @@ import io.cucumber.spring.ScenarioScope;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Configuration
 public class DriverManager {
@@ -29,16 +35,30 @@ public class DriverManager {
     private int browserHeight;
     @Value("${timeout}")
     private int timeout;
-    private final Duration TIMEOUT = Duration.ofSeconds(timeout);
+    @Value("${remote.grid.url}")
+    private String remoteGridUrl;
+    @Value("${remote.execution}")
+    private Boolean remoteExecution;
 
     @Bean(destroyMethod = "quit")
     @ScenarioScope
     public WebDriver driver() throws Exception {
-        return driverFactory.createDriver(browser,browserHeadless, browserWidth, browserHeight);
+        if (remoteExecution){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments(String.format("--window-size=%d,%d", browserWidth, browserHeight));
+            options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+                put("enableVideo", true);
+                put("enableVNC", true);
+            }});
+            //return new RemoteWebDriver(URI.create("http://192.168.1.15:4444/").toURL(), chromeOptions);
+            return new RemoteWebDriver(URI.create("http://192.168.1.15:4444/wd/hub").toURL(), options);
+        }else{
+            return driverFactory.createDriver(browser,browserHeadless, browserWidth, browserHeight);
+        }
     }
 
     @Bean
     public WebDriverWait waitFor() throws Exception {
-        return new WebDriverWait(driver(), TIMEOUT);
+        return new WebDriverWait(driver(), Duration.ofSeconds(timeout));
     }
 }
